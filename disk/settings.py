7fbf040803554838bc5ae4fbcc8c9a2be8f832ba
Роман Колеров <coder8080@mail.ru>
2020-08-30 16:33:23 +0300

    Доработана инструкция по запуску проекта

--- disk/settings.py
+++ disk/settings.py
@@ -128,9 +128,9 @@ LOGIN_REDIRECT_URL = "/"
 
 # Конфигурируем стаические файлы
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [STATIC_DIR]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_DIR = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 SITE_ID = 1
 
