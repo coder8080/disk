d6e1e13e557abf8e5b5e7cb1755086b236b2b8ca
Колеров Роман <coder8080@mail.ru>
2021-01-21 16:14:08 +0300

    Добавлена неоновая тема

--- static/js/theme-script.js
+++ static/js/theme-script.js
@@ -5,13 +5,14 @@ theme_input.addEventListener("change", e => {
     let csrf_token = document.querySelector("input[name=csrfmiddlewaretoken]").value
     let form_data = new FormData()
     form_data.append("csrfmiddlewaretoken", csrf_token)
-    if (theme_input.checked === true) {
-        form_data.append("theme", "dark")
-        theme_input.checked = true
-    } else {
-        form_data.append("theme", 'light')
-        theme_input.checked = false
-    }
+    form_data.append("theme", theme_input.value)
+    // if (theme_input.checked === true) {
+    //     form_data.append("theme", "dark")
+    //     theme_input.checked = true
+    // } else {
+    //     form_data.append("theme", 'light')
+    //     theme_input.checked = false
+    // }
     fetch('/change-theme/', {
         method: 'POST',
         body: form_data
